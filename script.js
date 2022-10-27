fa682b1890768b6f7817ad64c2b4159d3ab19d79
Ruben Villagrana <RubenVillagrana35@gmail.com>
2022-10-26 20:52:29 -0700

    6 is true

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ console.log(5 < 10)
 
 // 6. Write code to output the result of the comparison:
 // 10 is greater than 5.
+console.log( 10 > 5)
 
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
